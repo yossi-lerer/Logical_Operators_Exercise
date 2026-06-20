af1926c5350da1f8bdc896404d200880bab3d528
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-20 21:47:14 +0300

    finish the step 5

--- app.py
+++ app.py
@@ -12,3 +12,6 @@ print(f"status {not status}")
 age = 20
 has_id = True
 print(f"the person can enter {age >= 18 and has_id == True}")
+# step 5
+level = 3
+print(f"the level is between 1 and 5 {level >= 3 and level <= 5}")
