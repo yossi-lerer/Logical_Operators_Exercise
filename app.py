f842eed4c46bed8f62c7fba0b574c4427462169f
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-20 21:44:40 +0300

    finish the step 4

--- app.py
+++ app.py
@@ -8,3 +8,7 @@ print(f"is online or has access {is_online or has_access}")
 # step 3
 status = False
 print(f"status {not status}")
+# step 4
+age = 20
+has_id = True
+print(f"the person can enter {age >= 18 and has_id == True}")
