16ca0dc48281118e282a971e24ac7e92c6361d4f
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-20 21:40:24 +0300

    finish the step 2

--- app.py
+++ app.py
@@ -2,5 +2,6 @@
 # step 1
 is_online = True
 has_access = False
-print(f"is online {is_online}")
-print(f"has access {has_access}")
+print(f"is online {is_online}. has access {has_access}")
+# step 2
+print(f"is online or has access {is_online or has_access}")
